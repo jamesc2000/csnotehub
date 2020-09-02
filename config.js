6076a10a91098e5b8ac763de83a1c00eaa5a137a
James Cruz <cruz.james99@gmail.com>
2020-09-02 20:42:20 +0800

    + Initial commit

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'coffeecs',
+  subtitle: 'Tips for CS, IT, and CpE university students!',
+  copyright: '© James Laurence Cruz 2020',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'coffeecs',
+    photo: '/photo.jpg',
+    bio: '',
+    contacts: {
+      email: 'jlacruz2019@plm.edu.ph',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
