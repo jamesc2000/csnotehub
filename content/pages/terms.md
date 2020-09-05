5f5caf743833135dae40c4ca0d4ef93d0041880c
James Cruz <cruz.james99@gmail.com>
2020-09-05 16:33:30 +0800

    + Implemented logo + Fixed .gitattributes to not convert line endings + Centered logo on sidebar

--- content/pages/terms.md
+++ content/pages/terms.md
@@ -1,7 +1,7 @@
 ---
-title: "Contact me"
+title: "Terms and Conditions"
 template: "page"
-socialImage: "/media/image-4.jpg"
+socialImage: "/media/logo.svg"
 ---
 
 Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.
--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -1,39 +1,28 @@
 // @flow strict
-import React from 'react';
-import { withPrefix, Link } from 'gatsby';
-import styles from './Author.module.scss';
+import React from "react";
+import { withPrefix, Link } from "gatsby";
+import styles from "./Author.module.scss";
 
 type Props = {
   author: {
     name: string,
     bio: string,
-    photo: string
+    photo: string,
   },
-  isIndex: ?boolean
+  isIndex: ?boolean,
 };
 
 const Author = ({ author, isIndex }: Props) => (
-  <div className={styles['author']}>
+  <div className={styles["author"]}>
     <Link to="/">
       <img
         src={withPrefix(author.photo)}
-        className={styles['author__photo']}
-        width="75"
-        height="75"
-        alt={author.name}
+        className={styles['author__logo']}
+        width="125"
+        height="125"
+        alt="csnotehub Logo"
       />
     </Link>
-
-    { isIndex === true ? (
-      <h1 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
-      </h1>
-    ) : (
-      <h2 className={styles['author__title']}>
-        <Link className={styles['author__title-link']} to="/">{author.name}</Link>
-      </h2>
-    )}
-    <p className={styles['author__subtitle']}>{author.bio}</p>
   </div>
 );
 
