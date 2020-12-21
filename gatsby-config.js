f6032738ae508e17c9b8f548384c4d03865fd98c
James Cruz <cruz.james99@gmail.com>
2020-12-21 18:20:52 +0800

    + Now using inlineCodeMarker for inline markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,12 @@ module.exports = {
             options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
           },
           'gatsby-remark-autolink-headers',
-          'gatsby-remark-prismjs',
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: "›",
+            },
+          }, 
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           'gatsby-remark-external-links'
